03fb62f246c206eed6e257389fb9afba32ccb93b
20161104606 <1428373962@qq.com>
2018-10-04 12:53:09 +0800

    10.04
    修改后

--- sketch_oct03e_lianggechaosheng.ino
+++ sketch_oct03e_lianggechaosheng.ino
@@ -1,13 +1,7 @@
 #include <Servo.h>
 Servo myservo;
-#define TrigPin1 A0
-#define EchoPin1 A1
-#define TrigPin2 A2
-#define EchoPin2 A3
-#define TrigPin3 A4
-#define EchoPin3 A5
-#define TrigPin4 A6
-#define EchoPin4 A7
+#define TrigPin1 5
+#define EchoPin1 6
 float cm;
 float cm1;
 void setup()
@@ -15,12 +9,6 @@ void setup()
   Serial.begin(9600);
   pinMode(TrigPin1, OUTPUT);
   pinMode(EchoPin1, INPUT);
-  pinMode(TrigPin2, OUTPUT);
-  pinMode(EchoPin2, INPUT);
-  pinMode(TrigPin3, OUTPUT);
-  pinMode(EchoPin3, INPUT);
-  pinMode(TrigPin4, OUTPUT);
-  pinMode(EchoPin4, INPUT);
   myservo.attach(30);
 }
 void chaosheng1()
